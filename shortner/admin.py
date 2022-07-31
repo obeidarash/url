6eb3d0de52241610911516a0de05ca782d603f38
obeidarash <obeid75arash@gmail.com>
2022-07-31 17:29:19 +0430

    change lots of thins in admin of shortner app

--- shortner/admin.py
+++ shortner/admin.py
@@ -6,3 +6,18 @@ from .models import Url
 class UrlAdmin(admin.ModelAdmin):
     ordering = ['-create']
     list_display = ['url', 'uuid', 'create']
+    readonly_fields = ['url', 'uuid', 'create']
+    list_filter = ['create', ]
+    search_fields = ['url', ]
+    list_per_page = 50
+
+    search_help_text = 'Search in urls'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
