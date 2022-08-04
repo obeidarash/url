17d8575d6aab3328d29e73388efab76fcd70b93e
obeidarash <obeid75arash@gmail.com>
2022-08-04 14:57:29 +0430

    add Recapcha v2 to the admin login form

--- url/settings.py
+++ url/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -16,10 +15,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+RECAPTCHA_PRIVATE_KEY = '6LeAD0ghAAAAABKwaGJEO5SlTmQicYS99i8yoAat'
+RECAPTCHA_PUBLIC_KEY = '6LeAD0ghAAAAAEbXMNiQOOj9XlwK3COf5N4IiKgW'
 
-# Application definition
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'recaptcha2',
+}
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
+    'snowpenguin.django.recaptcha2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -60,7 +65,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'url.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -70,7 +74,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -90,7 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -101,7 +103,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
